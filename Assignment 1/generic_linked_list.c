10d5bf38d55280d806e61ec6b60af23ed66e5eea
Abhiroop25902 <abhiroop.m25902@gmail.com>
2022-07-29 20:05:22 +0530

    [ADD] v1 copied from past

---
+++ Assignment 1/generic_linked_list.c
@@ -0,0 +1,189 @@
+//Write Function to support generic linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node //definition of node
+{
+    void *data;
+    struct node *next;
+
+} node;
+
+typedef struct gll //definition of gll
+{
+    int dsize;
+    struct node *list;
+    int (*compareFn)(void *a, void *b);
+    void (*printFn)(void *a);
+} gll;
+
+typedef struct gll *genericLL;
+
+genericLL createGLL(int dsize, int (*cfunction)(void *, void *)) //makes and inserts node
+{
+    genericLL g;
+    g = (gll *)malloc(sizeof(gll));
+
+    g->dsize = dsize;
+    g->list = NULL;
+    g->compareFn = cfunction;
+
+    return g;
+}
+
+void addNodeGLL(genericLL g, void *d) //add node in the end
+{
+    node *temp = (node *)malloc(sizeof(node));
+    temp->data = malloc(g->dsize);
+    memcpy(temp->data, d, g->dsize);
+    temp->next = NULL;
+
+    if (g->list == NULL)
+        g->list = temp;
+    else
+    {
+        node *temp2 = g->list;
+
+        while (temp2->next != NULL)
+            temp2 = temp2->next;
+
+        temp2->next = temp;
+    }
+}
+
+void addNodeGLLSorted(genericLL g, void *d) //add node to make ascending order
+{
+    node *temp = (node *)malloc(sizeof(node));
+    temp->data = malloc(g->dsize);
+    memcpy(temp->data, d, g->dsize);
+    temp->next = NULL;
+
+    if (g->list == NULL)
+    {
+        g->list = temp;
+    }
+    else
+    {
+        if (g->compareFn(temp->data, g->list->data) == -1)
+        {
+            temp->next = g->list;
+            g->list = temp;
+        }
+        else
+        {
+            node *temp2 = g->list;
+
+            while (!(((g->compareFn(temp2->data, temp->data) == -1) || g->compareFn(temp2->data, temp->data) == 0) && g->compareFn(temp2->next->data, temp->data) == 1))
+                temp2 = temp2->next;
+
+            temp->next = temp2->next;
+            temp2->next = temp;
+        }
+    }
+}
+
+int isPresentGLL(genericLL g, void *d) //checks of value present in node
+{
+    node *temp = g->list;
+
+    int pos = 1;
+    int is_found = 0;
+
+    while (temp != NULL)
+    {
+        if (g->compareFn(temp->data, d) == 0)
+        {
+            is_found = 1;
+            return pos;
+        }
+
+        pos++;
+        temp = temp->next;
+    }
+
+    if (is_found == 0)
+        return 0;
+}
+
+int deleteNodeGLL(genericLL g, void *d) //deletes node
+{
+    node *temp = g->list;
+    int pos = 1;
+
+    while (temp != NULL)
+    {
+        if (g->compareFn(temp->data, d) == 0)
+        {
+            if (temp == g->list)
+                g->list = g->list->next;
+            else
+            {
+                node *temp2 = g->list;
+                while (temp2->next != temp)
+                    temp2 = temp2->next;
+
+                temp2->next = temp->next;
+            }
+
+            free(temp);
+            return pos;
+        }
+        temp = temp->next;
+        pos++;
+    }
+    return 0;
+}
+
+int getNodeDataGLL(genericLL g, int i, void *d) //give output of ith node
+{
+    int pos = 1;
+    node *temp = g->list;
+
+    while (temp != NULL && pos != i)
+    {
+        temp = temp->next;
+        pos++;
+    }
+    if (temp == NULL)
+        return 0;
+
+    memcpy(d, temp->data, g->dsize);
+    return 1;
+}
+
+
+int int_compare(void* a, void* b){
+    return *((int*)a) - *((int*)b);
+}
+
+void main()
+{
+    genericLL g = createGLL(sizeof(char *), strcmp); //makes stack
+
+    int a = 1;
+    addNodeGLL(g, &a);
+    printf("list = 1\n");
+
+    int b = 3;
+    addNodeGLL(g, &b);
+    printf("list = 1->3\n");
+
+    int c = 2;
+    addNodeGLLSorted(g, &c);
+    printf("list = 1->2->3\n");
+
+    int d = 4;
+    addNodeGLL(g, &d);
+    printf("list = 1->2->3->4\n");
+
+    printf("isPresentNode for 2: %d\n", isPresentGLL(g, &c));
+
+    printf("deleteNodeGLL() for 3 : %d\n", deleteNodeGLL(g, &b));
+    printf("list = 1->2->4\n");
+
+    int *ptr = (int *)malloc(sizeof(int));
+    getNodeDataGLL(g, 3, (void *)ptr);
+    printf("getNodeDataGLL for 3: %d\n", *ptr);
+}
